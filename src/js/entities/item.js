684bc6725030ffeb7d4c7e5acbc8bd70e3544ef1
Sergejs Sablinskis <sergejs.sablinskis@ms-idi.eu>
2017-10-03 12:22:36 +0300

    added fix to dates timezone

--- src/js/entities/item.js
+++ src/js/entities/item.js
@@ -29,7 +29,7 @@
 
             function parseMySQLDate(mysqlDate) {
                 var d = (mysqlDate || '').toString().split(/[- :]/);
-                return new Date(d[0], d[1] - 1, d[2], d[3], d[4], d[5]);
+                return new Date( Date.UTC( d[0], d[1] - 1, d[2], d[3], d[4], d[5] ));
             }
         };
 
